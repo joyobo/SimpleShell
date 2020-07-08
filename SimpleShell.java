47be4e337dd2d015ed807e07e4bc45b299d4e232
joyobo <joeyyeokl@gmail.com>
2020-07-08 18:27:35 +0800

    Skeleton of shell

---
+++ SimpleShell.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class SimpleShell{
+    public static void main(String[] args) throws IOException{
+        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+
+        while(true){
+            System.out.print("SimpleShell>>");
+            String cmdLine = console.readLine();
+
+            if(cmdLine.equals("exit")) break;
+            else if(cmdLine.equals("")) continue;
+            else if(cmdLine.equals("help")){
+                System.out.println();
+                System.out.println("____________________________");
+                System.out.println("|  Welcome to Simple Shell  |");
+                System.out.println("|___________________________|");
+                System.out.println();
+                System.out.println("Commands:");
+                System.out.println("1) exit ");
+                System.out.println();
+            }
+            else System.out.println("Invalid command. Type 'help' to see a list of commands.");
+
+        }
+    }
+}
